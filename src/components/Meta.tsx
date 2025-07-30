91c0a0127ed3d87c57bf1fd5be98777df9552e2d
sechanglee <peerscc@gmail.com>
2025-07-30 23:43:08 +0900

    SEO 최적화를 위한 OG 태그 추가

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -3,21 +3,39 @@ import { useEffect } from 'react';
 interface MetaProps {
   title: string;
   description: string;
+  url?: string;
+  image?: string;
 }
 
-const Meta = ({ title, description }: MetaProps) => {
+const Meta = ({ title, description, url = window.location.href, image }: MetaProps) => {
   useEffect(() => {
+    // 일반 메타 태그
     document.title = title;
-    const metaDescription = document.querySelector('meta[name="description"]');
-    if (metaDescription) {
-      metaDescription.setAttribute('content', description);
+    setMetaTag('name', 'description', description);
+
+    // Open Graph 메타 태그
+    setMetaTag('property', 'og:title', title);
+    setMetaTag('property', 'og:description', description);
+    setMetaTag('property', 'og:type', 'website');
+    setMetaTag('property', 'og:url', url);
+    if (image) {
+      setMetaTag('property', 'og:image', image);
     } else {
-      const meta = document.createElement('meta');
-      meta.name = 'description';
-      meta.content = description;
-      document.head.appendChild(meta);
+      setMetaTag('property', 'og:image', "/static/media/logo.2ee42c9195554c239024b933240dd3df.svg");
     }
-  }, [title, description]);
+
+    function setMetaTag(attr: 'name' | 'property', key: string, content: string) {
+      let element = document.querySelector(`meta[${attr}="${key}"]`);
+      if (element) {
+        element.setAttribute('content', content);
+      } else {
+        const meta = document.createElement('meta');
+        meta.setAttribute(attr, key);
+        meta.setAttribute('content', content);
+        document.head.appendChild(meta);
+      }
+    }
+  }, [title, description, url, image]);
 
   return null;
 };
